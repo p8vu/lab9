f0563a345a9f9602511c33fc397359d5a74fc1e7
Patricia Vu <p8vu@ucsd.edu>
2018-03-08 09:50:52 -0800

    fixed bug to make it highlight

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -15,11 +15,12 @@ function onServerResponse(data){
     var labElementId = getLabElementId(data.id);
     var labElement = $(labElementId);
     labElement.css({'background': 'yellow'});
+    console.log(labElement);
 }
 
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
